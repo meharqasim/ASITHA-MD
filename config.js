8c7e1dfaa0a86861cfbc6ba59baec80d595774f2
meharqasim <mstrqasim111@gmail.com>
2024-10-16 14:32:17 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=T6hAgI4A#kyMIl150D1auPLEETnxYrUZB-l1GiXGaNMIDgXzEoNc",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",//enter mongo db url
 };
